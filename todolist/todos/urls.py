67ef7f4771b52be0f14b36763bc86d862619e4c1
tidjungs <waewprach.s@ku.th>
2017-08-23 21:21:59 +0700

    add add method and config admin

--- todolist/todos/urls.py
+++ todolist/todos/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^details/(?P<id>\w{0,50})/$', views.details, name='details')
+    url(r'^details/(?P<id>\w{0,50})/$', views.details, name='details'),
+    url(r'^add', views.add, name='add')
 ]
--- todolist/todos/views.py
+++ todolist/todos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import Todo
@@ -10,3 +10,15 @@ def index(request):
 def details(request, id):
   todo = Todo.objects.get(id=id)
   return render(request, 'details.html', { 'todo': todo })
+
+def add(request):
+  if(request.method == 'POST'):
+    title = request.POST['title']
+    text = request.POST['text']
+    
+    todo = Todo(title=title, text=text)
+    todo.save()
+
+    return redirect('/todos')
+  else:
+    return render(request, 'add.html')
