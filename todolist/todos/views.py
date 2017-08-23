9eeb6d7688d6c073107bc86e5cc01f87dc9cc98a
tidjungs <waewprach.s@ku.th>
2017-08-23 21:01:18 +0700

    add Details

--- todolist/todos/views.py
+++ todolist/todos/views.py
@@ -6,3 +6,7 @@ from .models import Todo
 def index(request):
   todos = Todo.objects.all()
   return render(request, 'index.html', { 'todos': todos })
+
+def details(request, id):
+  todo = Todo.objects.get(id=id)
+  return render(request, 'details.html', { 'todo': todo })
